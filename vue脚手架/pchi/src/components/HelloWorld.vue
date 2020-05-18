495f00381d5ebdbfd7e79d6d3b03d1d3d099f23f
Chenxipei <1468159931@qq.com>
2020-05-18 16:37:53 +0800

    这是在学校测试玩玩的

---
+++ vue脚手架/pchi/src/components/HelloWorld.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="hello">
+   <input type="text" v-model="msg" @keydown.enter="enter"/>
+   <p class="red">
+   		{{toupper}}
+   </p>
+   <p>
+   	接受子:
+   </p>
+   <child :inpmsg="msg" @cval="pfn"></child>
+  </div>
+</template>
+
+<script>
+import child from "./child"
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'HelloWorld'
+    }
+  },
+  computed:{
+  	toupper(){
+  		return this.msg.toUpperCase()
+  	}
+  },
+  methods:{
+  	enter(){
+  		alert(this.msg)
+  	},
+  	pfn(val){
+  		console.log(val)
+  	}
+  },
+  components:{
+  	child
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.red{
+	color: red;
+}
+</style>
---
+++ vue脚手架/pchi/src/components/child.vue
@@ -0,0 +1,30 @@
+<template>
+	<div>
+		<p>
+			这是子组件接收到父的值：{{inpmsg}}
+		</p>
+		<p>
+			<button @click="fn">子传父</button>
+		</p>
+	</div>
+</template>
+<script>
+	export default{
+		props:["inpmsg"],
+		data(){
+			return{
+				cmsg:"子的值"
+			}
+		},
+		methods:{
+			fn(){
+				this.$emit("cval",this.cmsg)
+			}
+		}
+	}
+</script>
+<style>
+	p{
+		color: blue;
+	}
+</style>
